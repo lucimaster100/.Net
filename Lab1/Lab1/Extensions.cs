46fff8475da51b7f9e94fe895310000e92e5cad8
Cristian Fartade <fartade.cristian@gmail.com>
2021-10-04 16:29:57 +0300

    update

---
+++ Lab1/Lab1/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+   public static class Extensions
+    {
+        public static void ComputeWords(this string sentence)
+        {
+            string[] words = sentence.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+            Console.WriteLine("#The sentence contains " + words.Length + " words");
+        }
+    }
+}
--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -6,7 +6,19 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            Employee architect = new Architect();
+            Console.WriteLine("1.");
+            Employee architect = new Architect(Guid.NewGuid(), "Ionut", "Popa", new DateTime(2010, 5, 21), new DateTime(2022, 6, 24), 5000);
+            Employee manager = new Manager(Guid.NewGuid(), "Andrei", "Ciobanu", new DateTime(2018, 3, 11), new DateTime(2020, 4, 16), 4500);
+            architect.Salutation();
+            Console.WriteLine("Is " + architect.GetType().Name.ToLower() + " " + architect.GetFullName() + " active? " + architect.IsActive());
+            manager.Salutation();
+            Console.WriteLine("Is " + manager.GetType().Name.ToLower() + " " + manager.GetFullName() + " active? " + manager.IsActive());
+
+
+            Console.WriteLine();
+            Console.WriteLine("2.");
+            string sentence = "The lazy brown fox doesn't jump over the dog";
+            sentence.ComputeWords();
         }
     }
 }
